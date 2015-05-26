229f72b3e6ecc6a2f77b276bd15b71a69feb1db9
Raziq <mohideen.syed@rocketmail.com>
2015-05-26 18:22:45 -0400

    Added second file

---
+++ Bottle/Adera/AderaPlaid.ino
@@ -0,0 +1,78 @@
+// Define the pins we're going to call pinMode on
+int amp = A1;  // Weight to this one
+int capRead = D7; //
+
+int analogInputWeight = 0;
+double weightValue = 0;
+double pills = 0;
+int apxPillCount = 0;
+int capStatus = 0;
+
+int previousCapStatus = 0;
+
+char capStr[64];
+char weightStr[64];
+char pillCountStr[64];
+
+unsigned long lastTime = 0UL;
+
+char thisID[64];
+
+// This routine runs only once upon reset
+void setup() {
+  Time.zone(5);
+  Spark.variable("pills", &pills, DOUBLE);
+  Spark.variable("apxPillCount", &apxPillCount, INT);
+  Spark.variable("capStatus", &capStatus, INT);
+  pinMode(amp, INPUT);
+  pinMode(capRead, INPUT);
+
+  Spark.deviceID().toCharArray(thisID, 64);
+  Spark.variable("coreID", &thisID, STRING);
+}
+
+// This routine gets called repeatedly, like once every 5-15 milliseconds.
+// Spark firmware interleaves background CPU activity associated with WiFi + Cloud activity with your code.
+// Make sure none of your code delays or blocks for too long (like more than 5 seconds), or weird things can happen.
+void loop() {
+
+  unsigned long now = millis();
+
+  capStatus = digitalRead(capRead);
+  analogInputWeight = analogRead(amp);
+  weightValue = (analogInputWeight * 3.308)/4096;
+
+  pills = weightValue;// - 0.56;//(0.156 + 0.260);//plaid bottle: - 0.56;
+  apxPillCount = constrain(pills * 120 / 1.62, 0, 120);//plaid bottle: 120 / 1.62
+
+
+  if(previousCapStatus != capStatus)
+  {
+    lastTime = now;
+    previousCapStatus = capStatus;
+  }
+
+  if (now-lastTime>1500UL && lastTime != 0)
+  {
+        lastTime = 0;
+        if(capStatus)//Cap on
+        {
+          sprintf(capStr,"%d",capStatus);
+          Spark.publish("capOn",capStr);
+
+          sprintf(pillCountStr,"%d",apxPillCount);
+          Spark.publish("apxPillCount",pillCountStr);
+
+        }
+        else//Cap off
+        {
+          sprintf(capStr,"%d",capStatus);
+          Spark.publish("capOff", capStr);
+
+        }
+
+
+  }
+  //delay(500);
+
+}
